e0c72069cf52238600cb6bc4af3a82a213281015
Divyanshu Kulshreshtha <101164039+DivyanshuKuls@users.noreply.github.com>
2024-04-13 09:12:23 +0000

    variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 455655
+let email = "dx@yopmail.com"
+var password = "789hjk"
+city = "Kolkata"
+let state // undefined will be printed
+
+// accountId = 90909 
+// Will get ==>  "TypeError: Assignment to constant variable." 
+//We can't reassign the value to const variable
+
+email = "dc@yopmail.com" // email value will be updated
+
+console.table([accountId, email, password, city, state])
