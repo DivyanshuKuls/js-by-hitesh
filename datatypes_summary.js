cfe97c0080424770de08cd6983d20b7f3a69cc01
Divyanshu Kulshreshtha <101164039+DivyanshuKuls@users.noreply.github.com>
2024-04-19 04:59:16 +0000

    datatpes summary

---
+++ datatypes_summary.js
@@ -0,0 +1,13 @@
+/* 
+There are two types of datatypes :
+    1. Primitive (Call by value)
+    2. Non-primitive or Reference (Call by reference)
+
+Datatypes are categorized based on how they are stored in memory and how they are accessed from memory.
+
+1. Primitive
+    7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+2. Non-primitive (Reference)
+    Array, Objects, Functions
+*/
